2286feb7652d5dd3abc00f9b973b5e6d16a7780c
Nurbek Asanbekov <nurbek.asanbekov@zensoft.io>
2017-05-05 00:11:28 +0600

    master commit

--- test.cpp
+++ test.cpp
@@ -1,8 +1,13 @@
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main()
 {
     std::string s = "test";
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    
     std::cout << s << std::endl;
 }
