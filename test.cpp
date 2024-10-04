d86ced030f29dda294f295afeaa5429d96364a50
knifemaster <emilasanbekov@gmail.com>
2024-10-04 13:32:12 +0600

    adding some feature

--- test.cpp
+++ test.cpp
@@ -1,15 +1,15 @@
 #include <vector>
 #include <iostream>
 #include <string>
-
+#include <unordered_set>
 int main()
 {
     int i = 10;
 
     std::string s = "test";
-    std::vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    
+    std::unordered_set<int> v;
+    for (size_t index = 0; index < 10; index++) {
+        v.insert(index);
+    }
     std::cout << s << i << std::endl;
 }
