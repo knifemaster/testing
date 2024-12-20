97e6eb98fd79ac7a1609fd8a46b65dc1eff5e5c6
knifemaster <emilasanbekov@gmail.com>
2024-12-20 20:46:48 +0600

    added swapnodes and weak_ptr

---
+++ swap_nodes.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+};
+
+static int recurse_depth = 0;
+
+
+ListNode* swapPairs(ListNode* head) {
+
+	
+	
+	if (head == nullptr) {
+		return new ListNode(0);
+	}
+
+	std::cout << "value " << head->val << std::endl;
+	ListNode* h = head->next;
+	//ListNode* temp = head->next;
+	//ListNode* temp_head = head;
+
+	if (recurse_depth % 2 == 0) {
+		ListNode* temp_next = head->next;
+		ListNode* temp_head = head;
+		
+		head = head->next;
+		head->next = temp_head;	
+	}
+	recurse_depth += 1;
+
+	while (h != nullptr) {
+		std::cout << "recurse depth" << recurse_depth << std::endl;
+		//std::cout << head->val << std::endl;
+		return swapPairs(h);
+	}
+	
+	return new ListNode(1);
+
+}
+
+ListNode* createListNode(ListNode* head) {
+	ListNode* node = new ListNode(head->val, head);
+
+	return node;
+
+}
+
+int main() {
+
+	ListNode * l1node = new ListNode(1);
+	ListNode * l2node = new ListNode(2);
+	ListNode * l3node = new ListNode(3);
+	ListNode * l4node = new ListNode(4);
+
+	
+	l1node->next = l2node;
+	l2node->next = l3node;
+	l3node->next = l4node;
+	l4node->next = nullptr;
+
+	swapPairs(l1node);
+
+
+	std::cout << l1node->next->next->next->val << std::endl;
+	return 0;
+
+}
