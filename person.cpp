ca92628591127359c4e38402a528e0ea98d2e611
knifemaster <emilasanbekov@gmail.com>
2025-03-19 22:56:09 +0600

    added copy constructor

--- person.cpp
+++ person.cpp
@@ -4,18 +4,22 @@
 
 
 class Person {
-	private:
-		std::string name;
-		int age;
-	
-	public:
-		Person() : name("Unknown"), age(0) {
-			std::cout << "default constructor"<< '\n';
-		}
+    private:
+        std::string name;
+        int age;
 
-		Person(std::string name, int age) : name(std::move(name), age(age)) {
-			std::cout << "parametrized constructor" << '\n';
-		}
+    public:
+        Person() : name("Unknown"), age(0) {
+            std::cout << "default constructor"<< '\n';
+        }
+
+        Person(std::string name, int age) : name(std::move(name), age(age)) {
+            std::cout << "parametrized constructor" << '\n';
+        }
+
+        Person(const Person& other) : name(other.name), age(other.age) {
+            std::cout << "copy constructor" << "\n";
+        }
 
 
 
