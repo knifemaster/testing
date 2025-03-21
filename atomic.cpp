dcafeb48ec4e6501e9ac7dcdd19f9502f195befd
knifemaster <emilasanbekov@gmail.com>
2025-03-21 18:27:52 +0600

    added memory_order_acuire and memory_order_release

--- atomic.cpp
+++ atomic.cpp
@@ -11,6 +11,15 @@ void thread_func() {
 
 }
 
+std::atomic<int> y(0);
+std::atomic<bool> ready(false);
+
+void thread_func2() {
+    y.store(42, std::memory_order_relaxed);
+    ready.store(true, std::memory_order_release);
+
+}
+
 int main() {
 
     std::thread t(thread_func);
@@ -18,6 +27,20 @@ int main() {
     t.join();
 
     std::cout << "Value: " << value << std::endl;
+
+    std::thread t2(thread_func2);
+    while (!ready.load(std::memory_order_acquire)) {
+        std::cout << "Hello world" << std::endl;        // waiting while ready not true
+    }
+
+    int value2 = y.load(std::memory_order_relaxed);
+    t2.join();
+    std::cout << "Value: " << value2 << std::endl;
+
+
+
+
+
 
     return 0;
 }
