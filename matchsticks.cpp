0bac191a5266f093bc34fa5604530f517cd650e6
knifemaster <emilasanbekov@gmail.com>
2025-03-21 21:29:22 +0600

    added backtrack function for matchsticks

--- matchsticks.cpp
+++ matchsticks.cpp
@@ -19,6 +19,31 @@ bool makesquare(std::vector<int>& matchsticks) {
 
     std::vector<int> sides(4, 0);
 
-
+    return backtrack(matchsticks, sides, 0, target);
 
 }
+
+bool backtrack(const std::vector<int>& matchsticks, std::vector<int>& sides, int index, int target) {
+
+    if (index == matchsticks.size()) {
+        return sides[0] == target && sides[1] == target && sides[2] == target && sides[3] == target;
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        if (sides[i] + matchsticks[index] <= target) {
+            sides[i] += matchsticks[index];
+
+            if (backtrack(matchsticks, sides, index + 1, target)) {
+                return true;
+            }
+
+            sides[i] -= matchsticks[index];
+            if (sides[i] == 0) 
+                break;
+        }
+    }
+
+    return false;
+}
+
+
