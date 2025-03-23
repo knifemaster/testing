68243650fade46ff6859699ab6250bd6ae5e5f0b
knifemaster <emilasanbekov@gmail.com>
2025-03-23 22:52:10 +0600

    added main function with call all operators

--- operator_overload.cpp
+++ operator_overload.cpp
@@ -90,7 +90,7 @@ class MyClass {
             return value + x;
         }
 
-        int operator int() const {
+        operator int() const {
             return value;
         }
 
@@ -104,5 +104,48 @@ class MyClass {
             return is;
         }
 
-
 };
+
+
+int main() {
+
+    MyClass a(10);
+    MyClass b(5);
+
+    MyClass c = a + b;
+    std::cout << "a + b = " << c << std::endl;
+
+    c = a - b;
+    std::cout << "a - b = " << c << std::endl;
+
+    c = a * b;
+    std::cout << "a * b = " << c << std::endl;
+
+    c = a / b;
+    std::cout << "a / b = " << c << std::endl;
+
+    std::cout << "a == b: " << (a == b) << std::endl;
+    std::cout << "a != b: " << (a != b) << std::endl;
+    std::cout << "a < b: " << (a < b) << std::endl;
+    std::cout << "a > b: " << (a > b) << std::endl;
+
+    ++a;
+    std::cout << "После ++a: " << a << std::endl;
+
+    --b;
+    std::cout << "После --b: " << b << std::endl;
+
+    std::cout << "a[0] = " << a[0] << std::endl;
+
+    std::cout << "a(5) = " << a(5) << std::endl;
+
+    int x = a;
+    std::cout << "Приведение к int: " << x << std::endl;
+
+    std::cout << "Введите новое значение для a: ";
+    std::cin >> a;
+    std::cout << "Новое значение a: " << a << std::endl;
+
+
+    return 0;
+}
