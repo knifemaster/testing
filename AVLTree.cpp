038d7912eb95a6ec755f104bb9d3a7ee8983e72f
knifemaster <emilasanbekov@gmail.com>
2025-03-25 23:39:13 +0600

    Added AVLTree structure

---
+++ AVLTree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+
+template<typename T>
+class AVLTree {
+    private:
+        struct Node {
+            T data;
+            int height;
+            std::shared_ptr<Node> left;
+            std::shared_ptr<Node> right;
+        
+            Node(T val) : data(val), height(1), left(nullptr), right(nullptr) {}
+        };
+
+        std::shared_ptr<Node> root;
+
+
+};
