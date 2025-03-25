d1b20e26edcb0c210c17095b4e7f5a262bf9e0ca
knifemaster <emilasanbekov@gmail.com>
2025-03-25 23:42:03 +0600

    added getBalance method for difference heights

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -27,4 +27,8 @@ class AVLTree {
             }
         }
 
+        int getBalance(std::shared_ptr<Node> node) {
+            return node ? getHeight(node->left) - getHeight(node->right) : 0;
+        }
+
 };
