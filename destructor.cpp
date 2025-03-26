7a0c5383a5dfc3674124e9fb981d0e31e4a6bd58
knifemaster <emilasanbekov@gmail.com>
2025-03-26 20:45:54 +0600

    testing destructor exceptions

--- destructor.cpp
+++ destructor.cpp
@@ -1,24 +1,61 @@
 #include <iostream>
 #include <exception>
+#include <optional>
+#include <memory>
 
 class ForException {
     public:
         ForException(int value) : value(value) {}
         ~ForException() noexcept(false) {
             throw std::exception();
-            std::cout << "after exception" << std::endl;
+            std::cout << "after exception" << "\n";
         }
     private:
         int value;
 };
 
+class S {
+    public:
+        S() {}
+        S(const S& s) {
+            std::cout << "S::S(const S&)" << "\n";
+        }
+        S& operator=(const S& s) {
+            std::cout << "S::operator=" << "\n";
+            return *this;
+        }
+        ~S() {
+            std::cout << "S::~S()" << "\n";
+        }        
+};
+
+void f1() {
+    S s = S(); // destructor calls
+    S* s2 = new S(); // destructor not calls
+    
+    std::unique_ptr<S> s_uniq = std::make_unique<S>();//destructor calls
+
+    throw "exception";
+    delete s2;
+}
+
+void f2() {
+    f1();
+}
+
 
 int main() {
 
     try {
+        f2();
+    } catch(...) {
+        std::cerr << "catch from function" << "\n";
+    }
+
+    try {
         ForException object(100);
     } catch (const std::exception& e) {
-        std::cerr << "exception catched " <<e.what() << std::endl;
+        std::cerr << "exception catched " <<e.what() << "\n";
     }
 
     int result;
@@ -32,11 +69,11 @@ int main() {
 
         result = a / b;
     } catch(const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
         result = 0; 
     }
 
-    std::cout <<  "result is " << result << std::endl;
+    std::cout <<  "result is " << result << "\n";
     
 
     return 0;
