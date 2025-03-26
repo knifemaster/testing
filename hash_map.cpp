19c4de607c244ad57fe13ec688b1128bd1e2628b
knifemaster <emilasanbekov@gmail.com>
2025-03-26 23:34:53 +0600

    added simple hash function

--- hash_map.cpp
+++ hash_map.cpp
@@ -8,3 +8,11 @@ class HashMap {
 private:
     static const int DEFAULT_CAPACITY = 16;
     static const double LOAD_FACTOR;
+
+    std::vector<std::list<std::pair<K, V>>> buckets;
+    int size;
+
+    int hash(const K& key) const {
+        std::hash<K> hasher;
+        return hasher(key) % buckets.size();
+    }
