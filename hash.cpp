24d4ed6373ea72812c6463cc4d5957649f219ce1
knifemaster <emilasanbekov@gmail.com>
2025-03-27 21:49:02 +0600

    added demonstrateHash function

--- hash.cpp
+++ hash.cpp
@@ -45,3 +45,18 @@ size_t customStringHash(const std::string& s) {
     }
     return hash;
 }
+
+
+void demonstrateHashes() {
+    std::string text = "Hello";
+    Person p{"Alice", 30};
+
+    // standart hashes
+    cout << "Хеш строки \"Hello\" (std::hash): " << std::hash<std::string>()(text) << endl;
+    cout << "Хеш строки \"Hello\" (custom):    " << customStringHash(text) << endl;
+
+    // custom user hashes
+    cout << "Хеш Person (std::hash): " << std::hash<Person>()(p) << endl;
+    cout << "Хеш Person (PersonHasher): " << PersonHasher()(p) << endl;
+}
+
