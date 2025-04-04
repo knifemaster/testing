6267651a116d27a8c1167567e34c3363c5cc1aa1
knifemaster <emilasanbekov@gmail.com>
2025-04-04 21:56:36 +0600

    added iterators sizes

--- String_View_prof.cpp
+++ String_View_prof.cpp
@@ -7,3 +7,18 @@
 #include <string_view> 
 
 
+
+class StringView {
+public:
+    // Типы
+    using value_type = char;
+    using pointer = char*;
+    using const_pointer = const char*;
+    using reference = char&;
+    using const_reference = const char&;
+    using const_iterator = const char*;
+    using iterator = const_iterator;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using reverse_iterator = const_reverse_iterator;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
