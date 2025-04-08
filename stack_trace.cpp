393a3b7e49f3be395ba07a38ce345af2f5c7f08b
knifemaster <emilasanbekov@gmail.com>
2025-04-08 20:31:07 +0600

    added stacktrace

---
+++ stack_trace.cpp
@@ -0,0 +1,38 @@
+#include <stacktrace>
+#include <string>
+#include <iostream>
+
+void print_function(int a, std::string& s) {
+    auto trace = std::stacktrace::current();
+    std::cout << a << s << std::endl;
+
+    std::cout << std::to_string(trace) << std::endl;
+
+}
+
+struct Stack {
+    Stack() {
+        auto trace = std::stacktrace::current();
+        std::cout << std::to_string(trace) << std::endl;
+    }
+};
+
+void f() {
+    Stack st;    
+}
+
+void g() {
+    f();    
+}
+
+int main() {
+        std::string s = "Hello world";
+        auto trace = std::stacktrace::current();
+        std::cout << std::to_string(trace) << std::endl;
+        print_function(23, s);
+
+        Stack stack;
+
+        f();
+
+}
