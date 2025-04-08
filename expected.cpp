5db1d63a69df2804a326fe6b5532081ee9c5248f
knifemaster <emilasanbekov@gmail.com>
2025-04-08 23:19:37 +0600

    added main function

--- expected.cpp
+++ expected.cpp
@@ -23,3 +23,27 @@ auto parse_number(std::string_view& str) -> std::expected<double, parse_error> {
     str.remove_prefix(end - begin);
     return retval;
 }
+
+
+int main()
+{
+    auto process = [](std::string_view str)
+    {
+        std::cout << "str: " << std::quoted(str) << ", ";
+        if (const auto num = parse_number(str); num.has_value())
+            std::cout << "value: " << *num << '\n';
+            // If num did not have a value, dereferencing num
+            // would cause an undefined behavior, and
+            // num.value() would throw std::bad_expected_access.
+            // num.value_or(123) uses specified default value 123.
+        else if (num.error() == parse_error::invalid_input)
+            std::cout << "error: invalid input\n";
+        else if (num.error() == parse_error::overflow)
+            std::cout << "error: overflow\n";
+        else
+            std::cout << "unexpected!\n"; // or invoke std::unreachable();
+    };
+
+    for (auto src : {"42", "42abc", "meow", "inf"})
+        process(src);
+}
