2189c739815d90ca4ddd5417d75af5ddad65c22c
knifemaster <emilasanbekov@gmail.com>
2025-04-08 23:18:28 +0600

    added parse_number function

--- expected.cpp
+++ expected.cpp
@@ -9,3 +9,17 @@ enum class parseJ_error {
     invalid_input,
     overflow
 };
+
+auto parse_number(std::string_view& str) -> std::expected<double, parse_error> {
+    const char* begin = str.data();
+    char* end;
+    double retval = std::strtod(begin, &end);
+
+    if (begin == end)
+        return std::unexpected(parse_error::invalid_input);
+    else if (std::isinf(retval))
+        return std::unexpected(parse_error::overflow);
+
+    str.remove_prefix(end - begin);
+    return retval;
+}
