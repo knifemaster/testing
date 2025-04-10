59c150e9ea9ed81c8c39235464cf88cb822d7099
knifemaster <emilasanbekov@gmail.com>
2025-04-10 17:57:52 +0600

    standart allocation 10000000 elements

--- allocator.cpp
+++ allocator.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <list>
-
+#include <chrono>
 
 int main() {
+    const int N = 10000000;
+
+    while (true) {
+        auto start = std::chrono::high_resolution_clock::now();
+        {
+            std::list<int> list;
+            for (int i = 0; i < N; ++i) {
+                list.emplace_back(i);
+            } 
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        double ms = std::chrono::duration<double, std::milli>(end - start).count();
+        std::cout << "Time ms: " << ms << std::endl;
+    }
 
 
-
-    return 0
+    return 0;
 }
