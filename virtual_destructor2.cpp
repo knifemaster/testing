6ae8795e86a6e01e5b518efc7e9d45906a58ad28
knifemaster <emilasanbekov@gmail.com>
2025-04-13 21:39:02 +0600

    added Base class

--- virtual_destructor2.cpp
+++ virtual_destructor2.cpp
@@ -3,3 +3,19 @@
 #include <exception>
 #include <vector>
 
+
+class Base {
+    public:
+        Base() {
+            std::cout << "Base ctor\n";
+        }
+
+        virtual void print() {
+            std::cout << "Base print\n";
+        }
+
+        virtual ~Base() {
+            std::cout << "Base dtor\n";
+        }
+
+};
