75718844e659670c04cd68e5f53fab75487ae15f
knifemaster <emilasanbekov@gmail.com>
2025-04-14 21:03:16 +0600

    added promise with void for notification

--- future.cpp
+++ future.cpp
@@ -3,6 +3,30 @@
 #include <thread>
 #include <chrono>
 
+
+void worker(std::promise<void> prom) {
+    std::cout << "Рабочий поток начал работу\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    prom.set_value();
+    std::cout << "Рабочий поток завершил работу\n";
+}
+
+
+int main() {
+    std::promise<void> prom;
+    std::future<void> fut = prom.get_future();
+
+    std::thread t(worker, std::move(prom));
+    fut.wait();
+    std::cout << "Основной поток получил уведомление\n";
+
+    t.join();
+    return 0;
+
+}
+
+
+/*
 int multiply(int x) {
     return x*x;
 }
@@ -31,4 +55,4 @@ int main() {
 
     return 0;
 }
-
+*/
