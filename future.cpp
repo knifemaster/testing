31db742af28606a294c10f5e40f1958603ec391d
knifemaster <emilasanbekov@gmail.com>
2025-04-14 20:44:19 +0600

    added promise in thread

--- future.cpp
+++ future.cpp
@@ -1,16 +1,32 @@
 #include <future>
 #include <iostream>
-
+#include <thread>
+#include <chrono>
 
 int multiply(int x) {
     return x*x;
 }
 
+void worker(std::promise<int> prom) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    prom.set_value(42);
+}
+
+
+
 int main() {
 
-    std::future<int> fut = std::async(&multiply, 10);
+    std::promise<int> prom;
+    std::future<int> fut = prom.get_future();
+    std::thread t(worker, std::move(prom));
 
-    std::cout << fut.get();
+    std::cout << "Результат: " << fut.get() << "\n";
+
+    t.join();
+
+
+    //std::future<int> fut = std::async(&multiply, 10);
+    //std::cout << fut.get();
 
 
     return 0;
