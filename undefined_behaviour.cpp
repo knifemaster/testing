a42937d5a565a63ebccbbd3802340c1337aed22f
knifemaster <emilasanbekov@gmail.com>
2025-04-16 22:47:14 +0600

    added ub nullptr

--- undefined_behaviour.cpp
+++ undefined_behaviour.cpp
@@ -7,5 +7,11 @@ int main() {
     int x = arr[5];  // UB: чтение за пределами массива
     arr[10] = 42;    // UB: запись за пределами массива
     std::cout << x << std::endl;
+
+    
+    //2. Разыменование нулевого указателя
+    int* ptr = nullptr;
+    int x2 = *ptr;  // UB: разыменование nullptr
+
     return 0;
 }
