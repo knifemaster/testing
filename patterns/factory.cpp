68d75b5aee35212c6cc99adc5f1569abd31d0ff4
knifemaster <emilasanbekov@gmail.com>
2025-04-18 13:27:39 +0600

    added ConcreteProductA

--- patterns/factory.cpp
+++ patterns/factory.cpp
@@ -9,3 +9,11 @@ class Product {
         virtual void Use = 0;
 
 };
+
+
+class ConcreteProductA : public Product {
+    public:
+        void Use() override {
+            std::cout << "Используется продукт А" << "\n";
+        }
+};
