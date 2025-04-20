c514a47c2657eff315be2ccef3cd9fa1277a6b83
knifemaster <emilasanbekov@gmail.com>
2025-04-20 16:54:00 +0600

    added ISubject class

--- patterns/observer.cpp
+++ patterns/observer.cpp
@@ -4,4 +4,18 @@
 #include <memory>
 
 
+class IObserver {
+    public:
+        virtual ~IObserver() = default;
+        virtual void update(const std::string& message) = 0;
 
+}
+
+
+class ISubject {
+    public:
+        virtual ~ISubject() = default;
+        virtual void attach(std::shared_ptr<IObserver> observer) = 0;
+        virtual void detach(std::shared_ptr<IObserver> observer) = 0;
+        virtual void notify() = 0;
+};
