0267c2d416da7a5c1a0ed41eb532d0fda600d390
knifemaster <emilasanbekov@gmail.com>
2025-04-24 00:05:10 +0600

    added crtp

--- crtp.cpp
+++ crtp.cpp
@@ -12,3 +12,15 @@ class Counter {
             std::cout << "Total objects: " << count << std::endl;
         }
 };
+
+template <typename T>
+int Counter<T>::count = 0;
+
+class MyClass : public Counter<MyClass> {};
+
+int main() {
+    MyClass a, b, c;
+    MyClass::PrintCount();
+    return 0;
+
+}
