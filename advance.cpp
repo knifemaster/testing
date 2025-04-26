297150e1452be8d8597e4155c13d759879d3291f
knifemaster <emilasanbekov@gmail.com>
2025-04-26 18:40:04 +0600

    added set with advance

--- advance.cpp
+++ advance.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
+#include <set>
 int main() {
 
-    std::vector<int> vi = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    auto it = vi.begin();
-
+    std::vector<int> vec_i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    auto it = vec_i.begin();
     std::advance(it, 5);
     std::cout << *it;
+
+
+    std::set<int> set_i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    auto set_it = set_i.begin();
+    std::advance(set_it, 9);
+    std::cout << *set_it << std::endl;
+
+
 
     return 0;
 }
