9d0f375ff31b25ea8e786a63fd2bd12413ef533f
knifemaster <emilasanbekov@gmail.com>
2025-04-26 18:34:44 +0600

    added vector with advance

--- advance.cpp
+++ advance.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 int main() {
+
+    std::vector<int> vi = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto it = vi.begin();
+
+    std::advance(it, 5);
+    std::cout << *it;
 
     return 0;
 }
