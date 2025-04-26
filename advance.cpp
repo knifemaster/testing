53eef375291cea9897315dc7a2361aad1b2fc918
knifemaster <emilasanbekov@gmail.com>
2025-04-26 18:41:22 +0600

    added list of advance

--- advance.cpp
+++ advance.cpp
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <list>
+
+
 int main() {
 
     std::vector<int> vec_i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -16,6 +19,10 @@ int main() {
     std::advance(set_it, 9);
     std::cout << *set_it << std::endl;
 
+    std::list<int> lst = {10, 20, 30, 40, 50};
+    auto lst_it = lst.begin();
+    std::advance(lst_it, 2);
+    std::cout << *lst_it << std::endl;
 
 
     return 0;
