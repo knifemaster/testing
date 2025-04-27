71d29dd59c3c9b76b3f9942afee7ba864fecf756
knifemaster <emilasanbekov@gmail.com>
2025-04-27 18:49:21 +0600

    added vector with next

--- next.cpp
+++ next.cpp
@@ -4,6 +4,11 @@
 
 int main() {
 
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    auto it = vec.begin();
+    auto new_it = std::next(it, 2);
+    std::cout << *new_it << std::endl;
+    std::cout << *it << std::endl;
 
 
     return 0;
