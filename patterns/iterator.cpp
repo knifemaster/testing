ac172544a098a3947341fe3cff94cc6d62a64e39
knifemaster <emilasanbekov@gmail.com>
2025-04-30 20:17:26 +0600

    added collection interface

--- patterns/iterator.cpp
+++ patterns/iterator.cpp
@@ -12,3 +12,9 @@ class Iterator {
         virtual GameCharacter* next() = 0;
 };
 
+
+class CharacterCollection {
+public:
+    virtual ~CharacterCollection() = default;
+    virtual std::unique_ptr<Iterator> createIterator() = 0;
+};
