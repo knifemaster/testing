2ee9039e3c9a3ee39b06abce3c6ccd3542c3081c
knifemaster <emilasanbekov@gmail.com>
2025-05-05 21:36:26 +0600

    added RingIterator and constructor

--- patterns/iterator2.cpp
+++ patterns/iterator2.cpp
@@ -8,6 +8,20 @@ public:
     std::vector<int> top{9, 9, 9, 9, 9, 9, 9, 9, 9};
     std::vector<int> down{9, 9, 9, 9, 9, 9, 9, 9, 9};
 
+    class RingIterator {
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = int;
+        using difference_type = std::ptrdiff_t;
+        using pointer = int*;
+        using reference = int&;
+
+        RingIterator(std::vector<int>& top, std::vector<int>& down, bool is_end = false)
+            : top_vec(top), down_vec(down), is_end(is_end) {
+            current_vec = &top_vec;
+            current_pos = 0;
+        }
+
 
 int main() {
 
