a254c6aa15079d6ca4b170069197efecbeb31887
knifemaster <emilasanbekov@gmail.com>
2025-05-06 22:09:29 +0600

    added templates

--- initializer_list.cpp
+++ initializer_list.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 #include <initializer_list>
 
-
+template<typename T>
 class MyContainer {
     public:
-        MyContainer(std::initializer_list<int> list) {
-            for (int num : list) {
+        MyContainer(std::initializer_list<T> list) {
+            for (const T& num : list) {
                 data.push_back(num);
             }
         }
 
         void print() {
-            for (int num : data) {
+            for (T& num : data) {
                 std::cout << num << " ";
             }
 
@@ -20,14 +20,14 @@ class MyContainer {
         }
 
     private:
-        std::vector<int> data;
+        std::vector<T> data;
 
 };
 
 
 int main() {
 
-    MyContainer container = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    MyContainer<int> container = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     container.print();
 
     return 0;
