797dcdb11c0df7e1780ba53ec89d8921b45f28bf
knifemaster <emilasanbekov@gmail.com>
2025-05-06 22:18:56 +0600

    added function sum with initializer_list

--- initializer_list.cpp
+++ initializer_list.cpp
@@ -5,17 +5,15 @@
 template<typename T>
 class MyContainer {
     public:
-        MyContainer(std::initializer_list<T> list) {
-        
-        data.reserve(list.size());
-
+        explicit MyContainer(std::initializer_list<T> list) noexcept {
+            data.reserve(list.size());
             for (const T& num : list) {
                 data.emplace_back(num);
             }
         }
 
         void print() {
-            for (T& num : data) {
+            for (const T& num : data) {
                 std::cout << num << " ";
             }
 
@@ -28,10 +26,23 @@ class MyContainer {
 };
 
 
+int sum(std::initializer_list<int> nums) {
+    int total = 0;
+    for (int num : nums) {
+        total += num;
+    }
+    return total;
+}
+
+
 int main() {
 
     MyContainer<int> container = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     container.print();
+
+    std::cout << sum({1, 2, 3, 4}) << "\n";
+    std::cout << sum({10, 20, 30}) << "\n";
+
 
     return 0;
 }
