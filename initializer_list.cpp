bb0c516d4f69bade6173d4dfe95d07fb11358ab3
knifemaster <emilasanbekov@gmail.com>
2025-05-06 22:05:25 +0600

    added MyContainer class

--- initializer_list.cpp
+++ initializer_list.cpp
@@ -3,6 +3,28 @@
 #include <initializer_list>
 
 
+class MyContainer {
+    public:
+        MyContainer(std::initializer_list<int> list) {
+            for (int num : list) {
+                data.push_back(num);
+            }
+        }
+
+        void print() {
+            for (int num : data) {
+                std::cout << num << " ";
+            }
+
+            std::cout << "\n";
+        }
+
+    private:
+        std::vector<int> data;
+
+};
+
+
 int main() {
 
 
