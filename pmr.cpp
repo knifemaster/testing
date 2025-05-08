7487e2f3b943b6315d43a861ce9db441211765ea
knifemaster <emilasanbekov@gmail.com>
2025-05-08 22:35:22 +0600

    added pmr map

--- pmr.cpp
+++ pmr.cpp
@@ -1,5 +1,6 @@
 #include <memory_resource>
 #include <iostream>
+#include <map>
 
 int main() {
     char buffer[1024];
@@ -18,8 +19,18 @@ int main() {
 
     std::pmr::synchronized_pool_resource pool_for_string;
     std::pmr::string str{"Hello, PMR!", &pool_for_string};
-
     std::cout << str << "\n";
+
+
+
+    std::pmr::unsynchronized_pool_resource pool_for_map;
+    std::pmr::map<int, std::pmr::string> my_map{&pool_for_map};
+    my_map[1] = "One";
+    my_map[2] = "Two";
+
+    for (const auto& [key, val] : my_map) {
+        std::cout << key << ": " << val << "\n";
+    }
 
 
 
