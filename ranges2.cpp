485f58989eb58aa1708b0a47f4fa1e286ce54f55
knifemaster <emilasanbekov@gmail.com>
2025-05-11 22:16:08 +0600

    added ranges::to convertion to deque

--- ranges2.cpp
+++ ranges2.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <string>
 #include <string_view>
-
+#include <deque>
 
 int main() {
 
@@ -16,9 +16,12 @@ int main() {
     auto result = vs | std::views::join | std::ranges::to<std::string>();
 
     //std::string res = vs | rv::join("-") | std::ranges::to<std::string>();
-
+    
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    std::deque<int> squared = numbers | rv::transform([](int n) { return n*n;}) | std::ranges::to<std::deque>();
 
     std::print("{}", result);
+    std::print("{}", squared);
 
     return 0;
 }
