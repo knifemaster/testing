de40cdc27e3797f7e450c42b8ea855a5c660a8de
knifemaster <emilasanbekov@gmail.com>
2025-05-11 21:45:55 +0600

    added include files and main function

---
+++ ranges2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <print>
+#include <vector>
+#include <ranges>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+
+int main() {
+
+
+
+    return 0;
+}
