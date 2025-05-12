f027e72f48fb3a9d18ec44d0fd9ce4a86371408c
knifemaster <emilasanbekov@gmail.com>
2025-05-12 16:59:31 +0600

    added structs of vector and transform it

--- ranges3.cpp
+++ ranges3.cpp
@@ -3,5 +3,34 @@
 #include <ranges>
 #include <string>
 #include <format>
+#include <print>
 
+struct Person {
+    std::string name;
+    int age;
+};
 
+int main() {
+
+    std::vector<Person> people = {{"Alice", 30}, {"Bob", 25}};
+
+    //std::print("{}", people);
+
+    auto modified_people = people
+        | std::views::transform([](Person& p) {
+            p.name += "mrs";
+            p.age += 20;
+            return p;
+        });
+
+    auto people_str = modified_people
+        | std::views::transform([](const Person& p) {
+            return std::format("{} : ({}) ", p.name, p.age);
+                })
+        | std::views::join
+        | std::ranges::to<std::string>();
+
+    std::cout << people_str << "\n";
+
+    return 0;
+}
