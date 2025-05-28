2b9704d01cc07e401d61513293bb7975c1780bfe
knifemaster <emilasanbekov@gmail.com>
2025-05-28 19:00:33 +0600

    added concrete strategy

--- patterns/strategy.cpp
+++ patterns/strategy.cpp
@@ -7,3 +7,19 @@ public:
     virtual ~PaymentStrategy() = default;
     virtual void pay(float amount) const = 0;
 };
+
+
+class CreditCardPayment : public PaymentStrategy {
+private:
+    std::string cardNumber;
+    std::string cvv;
+
+public:
+    CreditCardPayment(const std::string& cardNumber, const std::string& cvv)
+        : cardNumber(cardNumber), cvv(cvv) {}
+
+    void pay(float amount) const override {
+        std::cout << "Оплата " << amount << " руб. через кредитную карту "
+                  << cardNumber.substr(cardNumber.length() - 4) << std::endl;
+    }
+};
