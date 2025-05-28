b4bb5c2d4267c9c66028160a5b9a6cb5e991cbda
knifemaster <emilasanbekov@gmail.com>
2025-05-28 18:59:05 +0600

    added interface

--- patterns/strategy.cpp
+++ patterns/strategy.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <memory>
 #include <string>
+
+class PaymentStrategy {
+public:
+    virtual ~PaymentStrategy() = default;
+    virtual void pay(float amount) const = 0;
+};
